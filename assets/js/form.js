2d03e5f8d99f716b33826148cbe064ba6ff3bbb2
Reina2024 <simmsreina@gmail.com>
2024-07-06 12:24:18 -0400

    Form Js , blank fields rejected

---
+++ assets/js/form.js
@@ -0,0 +1,45 @@
+// Set Variables
+const userName = document.getElementById("name");
+const blogTitle = document.getElementById("title");
+const blogContent = document.getElementById("content");
+const submitBtn = document.getElementById("submit-btn");
+const msgDiv = document.querySelector("#msg");
+
+let blogs = [];
+
+// Feild Can't Be Empty
+function displayMessage(type, message) {
+    msgDiv.textContent = message;
+    msgDiv.setAttribute("class", type);
+  }
+
+  submitBtn.addEventListener("click", function (event) {
+    if (userName.value === "") {
+      event.preventDefault();
+      displayMessage("error", "Name cannot be blank");
+    } else if (blogTitle.value === "") {
+      event.preventDefault();
+      displayMessage("error", "Title cannot be blank");
+    } else if (blogContent.value === "") {
+      event.preventDefault();
+      displayMessage("error", "Content cannot be blank");
+    } else {
+      const storedblogs = JSON.parse(localStorage.getItem("blogs"));
+      console.log(storedblogs);
+      if (storedblogs !== null) {
+        blogs = storedblogs;
+      }
+    }
+//   Send Blog Data
+    const blogData = {
+      username: userName.value,
+      title: blogTitle.value,
+      content: blogContent.value,
+    };
+    blogs.push(blogData);
+    localStorage.setItem("blogs", JSON.stringify(blogs));
+  });
+   
+
+
+  
